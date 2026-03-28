fc028542654e9f35f309d161c9edd53d5d876f64
CG <computergallery@CGs-iMac.local>
2026-03-28 10:08:55 +0530

    refactor: replace AdminSeeder with SuperAdminSeeder in DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -9,7 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminSeeder::class,
+            SuperAdminSeeder::class,
             SettingsSeeder::class,
             IncentiveOfferSeeder::class,
         ]);
